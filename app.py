7d5d6754a98833a47c03c2cd71ebb21ebb6f2fc3
ALTAF HUSAIN <altafhusain4228@gmail.com>
2025-10-23 15:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pickle
 import streamlit as st
-import os
 import time
 from sklearn.datasets import load_iris
 
